aadaa35fcac6f35827b797cf23192de344dee606
Bilal Shaikh <bilalshaikhh339@gmail.com>
2025-09-27 17:21:20 +0500

    i

---
+++ app.js
@@ -0,0 +1,37 @@
+var arr = [10, 20, 30, 40, 50]
+arr.splice(2, 1)
+console.log(arr)
+
+// var arr2 = [1, 2, 3, 5]
+// arr2.splice(3, 0, 4)
+// console.log(arr2)
+
+// var arr3 = ['a', 'b', 'c', 'd']
+// arr3.splice(1, 1, "x")
+// console.log(arr3)
+
+// var arr4 = [100, 200, 300, 400]
+// var arr5 = arr4.slice(1, 4)
+// console.log(arr5)
+
+// var arr6 = [10,20,30,40,50]
+// var arr7 = arr6.slice(arr6.length-3,arr6.length)
+// console.log(arr7)
+
+// var arr8 = [10,20,30,40,50]
+// arr8.splice(1,3)
+// console.log(arr8)
+
+
+// var arr9 = [1,2,3]
+// var arr10 = [4,5,6]
+// var arr11 = arr9.concat(arr10)
+// // var arr11 = arr9.slice(0,3).concat(arr10.slice(0,3))
+// console.log(arr11)
+
+
+// var arr12 = [1,2,3,4]
+// var arr13 = arr12.slice(0,4)
+// arr13.splice(4,0,5)
+// console.log(arr13)
+
